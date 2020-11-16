500ab8c8c275c91d22506b77b995b234a56a18d1
MaksimKarpeshin <feap135@yandex.ru>
2020-11-16 23:13:37 +0300

    Урок 1.задание №3

--- src/ru/geekbrains/Lesson1/Main.java
+++ src/ru/geekbrains/Lesson1/Main.java
@@ -1,5 +1,10 @@
 package ru.geekbrains.Lesson1;
 
+/**
+ Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+ где a, b, c, d – аргументы этого метода, имеющие тип float.
+ */
+
 public class Main {
     public static void main(String[] args) {
         byte b = 1;
@@ -10,6 +15,13 @@ public class Main {
         double d = 4.596;
         char c = 5;
         boolean a = true;
+
+        work3(1,2,3,4);
+    }
+
+    public static void work3(float a, float b, float c, float d){
+        float e = a * (b + (c / d));
+        System.out.println(e);
     }
 
 }
