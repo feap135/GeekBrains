8d287575b77da2ef8b268881771966f6ccccb2dd
MaksimKarpeshin <feap135@yandex.ru>
2020-11-16 23:35:14 +0300

    Урок 1.задание №4

--- src/ru/geekbrains/Lesson1/Main.java
+++ src/ru/geekbrains/Lesson1/Main.java
@@ -1,8 +1,8 @@
 package ru.geekbrains.Lesson1;
 
 /**
- Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
- где a, b, c, d – аргументы этого метода, имеющие тип float.
+ * Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в предела
+ * х от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
  */
 
 public class Main {
@@ -16,13 +16,25 @@ public class Main {
         char c = 5;
         boolean a = true;
 
-        work3(1,2,3,4);
+        work3(1, 2, 3, 4);
+        work4(3,87);
     }
 
-    public static void work3(float a, float b, float c, float d){
+    public static void work3(float a, float b, float c, float d) {
         float e = a * (b + (c / d));
         System.out.println(e);
     }
 
+   public static boolean work4(int a, int b){
+        int x = a + b;
+        if (x >= 10 && x<=20) {
+            System.out.println(true);
+            return true;
+        }
+        else {
+            System.out.println(false);
+            return false;
+        }
+    }
 }
 
