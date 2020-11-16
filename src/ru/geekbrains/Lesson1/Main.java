6b5e176faa8afffe93c3413f1b5d9b5b359a956a
MaksimKarpeshin <feap135@yandex.ru>
2020-11-16 23:02:09 +0300

    Урок 1.задание №2

--- src/ru/geekbrains/Lesson1/Main.java
+++ src/ru/geekbrains/Lesson1/Main.java
@@ -1,8 +1,16 @@
 package ru.geekbrains.Lesson1;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        byte b = 1;
+        short s = 32;
+        int i = 25365;
+        long l = 73737267L;
+        float f = 5.4f;
+        double d = 4.596;
+        char c = 5;
+        boolean a = true;
     }
+
 }
+
